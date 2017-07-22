f38f4056753e737dc8b684cad3d9a9bbbd690de2
KadyrS <sultan.kadyrkesh@yandex.ru>
2017-07-22 19:03:13 +0600

    Сделали классы для Видео Файла

---
+++ VideoManager/Model/FIleCamera.cs
@@ -0,0 +1,77 @@
+﻿using System;
+
+namespace VideoManager.Model
+{
+    class FIleCamera
+    {
+        /// <summary>
+        /// Код какой-то, пока не знаю
+        /// </summary>
+        public int SecurityCode { get; set; }
+
+        /// <summary>
+        /// Системное имя файла
+        /// </summary>
+        public string SystemFileName { get; set; }
+
+        /// <summary>
+        /// Оригинальное имя файла
+        /// </summary>
+        public string OriginalFileName { get; set; }
+        
+        /// <summary>
+        /// Путь к файлу
+        /// </summary>
+        public string SourcePath { get; set; }
+        
+        /// <summary>
+        /// Дата загрузки файла
+        /// </summary>
+        public DateTime UploadDate { get; set; }
+
+        /// <summary>
+        /// Дата создания файла
+        /// </summary>
+        public DateTime CreateDate { get; set; }
+
+        /// <summary>
+        /// Классификация
+        /// </summary>
+        public Classification Classification { get; set; }
+
+        /// <summary>
+        /// Уровень секретности
+        /// </summary>
+        public SecurityLevel SecurityLevel { get; set; }
+
+        /// <summary>
+        /// Расширение файла
+        /// </summary>
+        public string FileType { get; set; }
+
+        /// <summary>
+        /// Идентификатор системы управления записью
+        /// </summary>
+        public long RMS_ID { get; set; }
+
+        /// <summary>
+        /// Компьютерный идентификатор отправки
+        /// </summary>
+        public long CAD_ID { get; set; }
+
+        /// <summary>
+        /// Рейтинг файла
+        /// </summary>
+        public Rating Rating { get; set; }
+
+        /// <summary>
+        /// Описание файла
+        /// </summary>
+        public string Description { get; set; }
+
+        /// <summary>
+        /// Размер файла
+        /// </summary>
+        public long Size { get; set; }
+    }
+}
